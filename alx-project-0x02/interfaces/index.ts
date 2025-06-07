519bb8e6c1c5bb0dd87c6045fc7954b2c67eb583
LeonNjoroge <leonmuraguri@gmail.com>
2025-06-07 10:17:28 +0300

    Feat: Update proj 0 task 5

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,4 +1,6 @@
 // interface page
+import React from "react";
+
 export interface CardProps {
         title: string;
         content: string;
@@ -9,3 +11,13 @@ export interface PostModalProps {
         onClose: () => void;
         onSubmit: (data: CardProps) => void;
 }
+
+export interface ButtonProps {
+        size: Size;
+        shape: Shape;
+        children: React.ReactNode;
+        onClick?: () => void;
+}
+
+export type Size = "small" | "medium" | "large";
+export type Shape = "rounded-sm" | "rounded-md" | "rounded-full";
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Button from "@/components/common/Button";
 
 const About: React.FC = () =>{
+
     return (
         <div className="p-10">
             <h1>About</h1>
+            <h2>Buttons</h2>
+            <div className="flex gap-7">
+                <Button size={"small"} shape={"rounded-sm"} >Rounded sm</Button>
+                <Button size={"medium"} shape={"rounded-md"} >Rounded md</Button>
+                <Button size={"large"} shape={"rounded-full"} >Rounded full</Button>
+            </div>
+
         </div>
     );
 }
