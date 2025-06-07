30001e8b354df6041dcd488f3c408e680d9fc322
LeonNjoroge <leonmuraguri@gmail.com>
2025-06-07 09:16:08 +0300

    Feat: Update proj 0 task 3

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {CardProps} from "@/interfaces";
 
-const Card: React.FC <CardProps> = ({title, content}) =>{
+const Card: React.FC <CardProps> = ({title, content} : CardProps) =>{
     return(
         <div className="flex flex-col gap-2 rounded-2xl border border-gray-300 bg-white p-5 shadow-md transition hover:shadow-lg hover:cursor-pointer">
             <h1 className="text-lg font-semibold text-gray-800">{title}</h1>
