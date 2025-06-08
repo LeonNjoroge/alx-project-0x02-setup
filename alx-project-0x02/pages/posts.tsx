2d8386cbb2acf26d22e03b707230bc1f69a72980
LeonNjoroge <leonmuraguri@gmail.com>
2025-06-08 13:55:01 +0300

    Feat: Update proj 0 task 8

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,40 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "@/components/layout/Header";
 import { type PostProps } from "@/interfaces";
 import PostCard from "@/components/common/PostCard";
 
 const Posts: React.FC <PostProps> = ({title,}) =>{
 
-    const sitePosts: PostProps[] = [
-        {
-            title: "First Post",
-            content: "This is the content of the first post.",
-            userId: 1,
-        },
-        {
-            title: "React Tips",
-            content: "Use hooks wisely and split your components properly.",
-            userId: 2,
-        },
-        {
-            title: "Weekly Update",
-            content: "Finished refactoring the codebase and improved performance.",
-            userId: 3,
-        },
-        {
-            title: "UI Overhaul",
-            content: "Redesigned the dashboard with Tailwind CSS.",
-            userId: 4,
-        },
-    ];
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(()=>{
+        fetch("https://my-json-server.typicode.com/typicode/demo/posts")
+            .then(res => res.json())
+            .then((data) => {
+                const formatted = data.map((post: any) => ({
+                    title: post.title,
+                    content: post.body,
+                    userId: `User-${post.id}`,
+                }));
+                setPosts(formatted);
+            });
+        }
+        , []);
 
     return (
         <div className="p-10">
             < Header />
             <h1>Posts</h1>
             <div className="flex gap-5">
-                {sitePosts.map((post, idx)=>(
-                    <PostCard title={post.title} content={post.content} userId={post.userId} />
+                {posts.map((post: PostProps, idx)=>(
+                    <PostCard key={idx} title={post.title} content={post.content} userId={post.userId} />
                 ))}
             </div>
         </div>
